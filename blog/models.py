acef3c47a5f523c0e52ec5af0b387ae0b29caf0c
Shinminji <tlsalswl0630@gmail.com>
2020-07-23 03:27:44 +0900

    unit15

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,15 @@
+from django.conf import settings
 from django.db import models
 
+
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     author_name = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
