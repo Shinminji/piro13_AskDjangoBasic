1a77146fe3ca8870bc53d97faf2adf1a30303770
Shinminji <tlsalswl0630@gmail.com>
2020-07-23 02:51:05 +0900

    unit13

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Item
 
 #view 호출에 대한 리턴값은 HttpResponse객체
 
 
 def archives_year(request, year):
     return HttpResponse('{}년도에 대한 내용'.format(year))
+
+def item_list(request):
+    qs = Item.objects.all()
+    q = request.GET.get('q','')
+    if q:
+        qs.filter(name__icontains=q)
+    return render(request, 'shop/item_list.html',{
+        'item_list':qs,
+        'q' : q,
+    })
