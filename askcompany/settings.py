c788a25d028e7e66719e14205b88d65fe8513724
Shinminji <tlsalswl0630@gmail.com>
2020-07-23 10:49:01 +0900

    Unit17

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'prp1#**k+3^3ipg%j+non7gn(_8a*5s9k7&$l$505x(gfk)9$j'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -76,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'askcompany.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
@@ -85,11 +82,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'mypostgres' : {
+    'mypostgres': {
 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -109,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -123,13 +118,44 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
 
-
 INTERNAL_IPS = [
-    '127.0.0.1',
-]
+    '127.0.0.1', ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'write_to_file': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.FileHandler',
+            'filename': 'db.log',
+        },
+    },
+    #로깅
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console', 'write_to_file'],
+            'level': 'DEBUG',
+        },
+        'shop':{
+            'handlers' : ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,12 @@
+import logging
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Item
 
+#__name__ ==> "shop.views"
+logger = logging.getLogger(__name__)
+
 #view 호출에 대한 리턴값은 HttpResponse객체
-
-
 def archives_year(request, year):
     return HttpResponse('{}년도에 대한 내용'.format(year))
 
@@ -13,6 +15,9 @@ def item_list(request):
     q = request.GET.get('q','')
     if q:
         qs.filter(name__icontains=q)
+
+    logger.debug('query : {}'.format(q))
+
     return render(request, 'shop/item_list.html',{
         'item_list':qs,
         'q' : q,
